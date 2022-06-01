0c872d2f72bdfa84a632edd49bb573d51c7b84c5
ruadoian <ruadoian@gmail.com>
2022-06-01 18:55:17 +0900

    mobile view fixed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      screens:{
+        sm:'480px',
+        md:'760px',
+        lg:'976px',
+        xl:'1440px'
+      }
+    },
+  },
+  plugins: [],
+}
